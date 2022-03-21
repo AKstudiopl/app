6fd02450a779a154624f6258e671b6a90ea7038b
AKstudiopl <akrylowicz5spt@gmail.com>
2022-03-21 11:05:27 +0100

    1.0.7

---
+++ Zesia/1.0.0/music-list.js
@@ -0,0 +1,93 @@
+var allMusic = [
+
+  {
+      name: "Havana",
+      artist: "Camila Cabello",
+      src: "CamilaCabelloHavana",
+      color: "linear-gradient(#6a1d05, #000000)"
+  },
+  {
+      name: "Dj Is Your Second Name",
+      artist: "C-Bool , Giang Pham",
+      src: "C-BooLDJ",
+      color: "linear-gradient(#CCCCCC, #000000)"
+  },
+  {
+      name: "Never Go Away",
+      artist: "C-Bool",
+      src: "C-BooL-NeverGoAway",
+      color: "linear-gradient(#a85b00, #000000)"
+  },
+  {
+      name: "Champions",
+      artist: "Kevin Rudolf",
+      src: "KevinRudolf-Champions",
+      color: "linear-gradient(#53081b, #000000)"
+  },
+  {
+      name: "Ride It",
+      artist: "DJ Regard",
+      src: "DJRegard-Rideit",
+      color: "linear-gradient(#9198e5, #000000)"
+  },
+  {
+      name: "Havana",
+      artist: "Camila Cabello",
+      src: "CamilaCabelloHavana",
+      color: "linear-gradient(#6a1d05, #000000)"
+  },
+  {
+      name: "Dj Is Your Second Name",
+      artist: "C-Bool , Giang Pham",
+      src: "C-BooLDJ",
+      color: "linear-gradient(#CCCCCC, #000000)"
+  },
+  {
+      name: "Never Go Away",
+      artist: "C-Bool",
+      src: "C-BooL-NeverGoAway",
+      color: "linear-gradient(#a85b00, #000000)"
+  },
+  {
+      name: "Champions",
+      artist: "Kevin Rudolf",
+      src: "KevinRudolf-Champions",
+      color: "linear-gradient(#53081b, #000000)"
+  },
+  {
+      name: "Ride It",
+      artist: "DJ Regard",
+      src: "DJRegard-Rideit",
+      color: "linear-gradient(#9198e5, #000000)"
+  },
+  {
+      name: "Havana",
+      artist: "Camila Cabello",
+      src: "CamilaCabelloHavana",
+      color: "linear-gradient(#6a1d05, #000000)"
+  },
+  {
+      name: "Dj Is Your Second Name",
+      artist: "C-Bool , Giang Pham",
+      src: "C-BooLDJ",
+      color: "linear-gradient(#CCCCCC, #000000)"
+  },
+  {
+      name: "Never Go Away",
+      artist: "C-Bool",
+      src: "C-BooL-NeverGoAway",
+      color: "linear-gradient(#a85b00, #000000)"
+  },
+  {
+      name: "Champions",
+      artist: "Kevin Rudolf",
+      src: "KevinRudolf-Champions",
+      color: "linear-gradient(#53081b, #000000)"
+  },
+  {
+      name: "Ride It",
+      artist: "DJ Regard",
+      src: "DJRegard-Rideit",
+      color: "linear-gradient(#9198e5, #000000)"
+  },
+]
