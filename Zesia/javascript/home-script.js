7e7e2050bcd4b3395371e551558ba4e9a6000892
AKstudiopl <akrylowicz5spt@gmail.com>
2022-03-25 23:58:14 +0100

    1.1.1

--- Zesia/javascript/home-script.js
+++ Zesia/javascript/home-script.js
@@ -1,8 +1,9 @@
 const menuBtn = document.getElementsByClassName('navbar-btn')[0]
 const menuScreen = document.getElementsByClassName('navbar-menu')[0]
-const contentHome = document.getElementsByClassName('content')[0]
+const menuBtnExit = document.getElementsByClassName('nav-menu-exit')[0]
 menuBtn.addEventListener('click', () => {
   menuScreen.classList.toggle('active')
-  contentHome.classList.toggle('active')
-  menuBtn.classList.toggle('active')
+})
+menuBtnExit.addEventListener('click', () => {
+  menuScreen.classList.toggle('active')
 })
