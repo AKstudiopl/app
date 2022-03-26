ae015aa21215d8af3a26f4ec09ac65ac3b969768
AKstudiopl <akrylowicz5spt@gmail.com>
2022-03-27 00:10:14 +0100

    1.1.2

--- Zesia/javascript/home-script.js
+++ Zesia/javascript/home-script.js
@@ -10,3 +10,11 @@ menuBtnExit.addEventListener('click', () => {
   menuScreen.classList.toggle('active')
   body.classList.toggle('active')
 })
+const slider = document.querySelector(".slider input");
+const img = document.querySelector(".images .img-2");
+const dragLine = document.querySelector(".slider .drag-line");
+slider.oninput = ()=>{
+  let sliderVal = slider.value;
+  dragLine.style.left = sliderVal + "%";
+  img.style.width = sliderVal + "%";
+}
