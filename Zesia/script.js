e39eb8bde008852e344efdeb3b378f6977fa31bd
AKstudiopl <akrylowicz5spt@gmail.com>
2022-04-02 16:11:13 +0200

    1.1.5.3

--- Zesia/script.js
+++ Zesia/script.js
@@ -673,14 +673,23 @@ function shortcutCheck() {
   })
 
   if ('mediaSession' in navigator) {
-  navigator.mediaSession.metadata = new MediaMetadata({
-    title: 'personalMusic[indexNumb - 1].name',
-    artist: 'personalMusic[indexNumb - 1].artist',
-    artwork: [
-      { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
-    ]
-  });
 
-  navigator.mediaSession.setActionHandler('previoustrack', prevMusic());
-  navigator.mediaSession.setActionHandler('nexttrack', nextMusic());
-}
+      navigator.mediaSession.metadata = new MediaMetadata({
+      title: 'Never Gonna Give You Up',
+      artist: 'Rick Astley',
+      album: 'Whenever You Need Somebody',
+      artwork: [
+          { src: 'https://dummyimage.com/96x96',   sizes: '96x96',   type: 'image/png' },
+          { src: 'https://dummyimage.com/128x128', sizes: '128x128', type: 'image/png' },
+          { src: 'https://dummyimage.com/192x192', sizes: '192x192', type: 'image/png' },
+          { src: 'https://dummyimage.com/256x256', sizes: '256x256', type: 'image/png' },
+          { src: 'https://dummyimage.com/384x384', sizes: '384x384', type: 'image/png' },
+          { src: 'https://dummyimage.com/512x512', sizes: '512x512', type: 'image/png' },
+      ]
+      });
+
+      navigator.mediaSession.setActionHandler('play', playMusic());
+      navigator.mediaSession.setActionHandler('pause', pauseMusic());
+      navigator.mediaSession.setActionHandler('previoustrack', prevMusic());
+      navigator.mediaSession.setActionHandler('nexttrack', nextMusic());
+  }
