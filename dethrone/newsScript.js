cd24c0e9cfe9100b3c91b0174cfe76a2a9ed2c52
AKstudiopl <akrylowicz5spt@gmail.com>
2022-07-19 21:41:29 +0200

    dethrone.com 1.0.3

--- dethrone/newsScript.js
+++ dethrone/newsScript.js
@@ -4,6 +4,8 @@ const blogsCount = document.querySelector(".blogs-counter");
 const hiddenPostsPopup = document.querySelector(".hiddenPostsAmount");
 const loadbtncontent = document.querySelector(".loadmorecontent");
 const fastBackupBtn = document.querySelector(".fastBackBtn");
+const loadItem = document.querySelector(".load-item");
+const readMoreBtn = document.querySelectorAll(".news-read-more-button");
 
 postsCount.textContent = $('#allNews > div').length;
 tutorialsCount.textContent = $('#allNews > .tutorial').length;
@@ -36,12 +38,15 @@ if (window.location.hash === "#pl") {
   attentionAlert.classList.remove('hidden');
   loadbtncontent.textContent = "Pokaż Więcej";
   fastBackupBtn.textContent = "Powrót";
+  readMoreBtn.forEach(readMoreSingleBtn => {
+    readMoreSingleBtn.textContent = "Pokaż Więcej";
+  })
 }
 
 const loadMorePostsBtn = document.getElementById('loadMoreBtn');
 const allPostsCheck = document.querySelectorAll('.top-news');
 
-function moreview(){
+function moreviewOld(){
   allPostsCheck.forEach(singlepost => {
     singlepost.classList.remove('hidden');
     hiddenPostsPopup.textContent = $('#allNews > .hidden').length;
@@ -53,3 +58,55 @@ function moreview(){
     }
   });
 }
+
+function moreview() {
+  const amount = 5;
+  const hiddenPosts = document.querySelectorAll(".top-news.hidden");
+  const remaining = hiddenPosts.length;
+  loadItem.classList.add("active");
+  if (remaining > 0) {
+    for (let i = 0; i < (remaining >= amount ? amount : remaining); i++) {
+      setTimeout(() => {
+        hiddenPosts[i].classList.remove("hidden");
+        hiddenPostsPopup.textContent = $('#allNews > .hidden').length;
+        if (hiddenPostsPopup.textContent === "0") {
+          loadbtncontent.textContent = "That's All";
+          if (window.location.hash === "#pl") {
+            loadbtncontent.textContent = "To Wszystko";
+          }
+        }
+      }, 300 * i);
+    }
+  }
+  setTimeout(function(){
+           loadItem.classList.remove("active");
+           if (hiddenPostsPopup.textContent === "0"){
+             loadItem.classList.add("active");
+           }
+     }, 2000);
+}
+
+function readMore(btn){
+  let post = btn.parentElement;
+  console.log(post)
+  post.parentElement.classList.toggle("read-more");
+  if (post.parentElement.classList.contains("read-more")) {
+    btn.textContent = "Read Less"
+    if (window.location.hash === "#pl") {
+      btn.textContent = "Pokaż Mniej"
+    }
+  }else{
+    btn.textContent = "Read More"
+    if(window.location.hash === "#pl"){
+        btn.textContent = "Pokaż Więcej"
+    }
+  }
+  }
+
+  function imageView(img){
+    let fullPost = img.parentElement;
+    console.log(fullPost)
+    if (fullPost.parentElement.classList.contains("read-more")) {
+      fullPost.parentElement.classList.toggle("fullscreen");
+    }
+  }
