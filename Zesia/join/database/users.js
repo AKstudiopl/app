c88061ba8943e0bcedb4d76634cc8deb8d982658
AKstudiopl <akrylowicz5spt@gmail.com>
2022-10-26 20:29:43 +0200

    1.4.4_4

---
+++ Zesia/join/database/users.js
@@ -0,0 +1,19 @@
+var users = [
+    {
+        mail: "example@gmail.com",
+        username: "Adam",
+        id: "1",
+        password: "Aezakmi",
+        type: "admin",
+
+        us_cntr: "",
+        us_statChange: "",
+        us_date: "Wednesday, 26.10.2022",
+        us_age: "",
+        us_soc: {
+            us_fb: "",
+            us_in: "",
+            us_tw: "",
+        },
+    },
+]
